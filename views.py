b471a2e7505ede50c3cf292f04762f3a0e3aaeb6
Takahashi-Kaisei <s2322037@stu.musashino-u.ac.jp>
2024-10-21 18:28:03 +0900

    login画面を実装．なんかエラーでる．

--- views.py
+++ views.py
@@ -1,14 +1,24 @@
 from flask import Flask, render_template, url_for
 from flask_bootstrap import Bootstrap
+from flask_wtf.csrf import CSRFProtect
+from forms import *
+import os
 
 app = Flask(__name__)
 bs = Bootstrap(app)
+app.config["SECRET_KEY"] = os.urandom(32)
+app.config["WTF_CSRF_SECRET_KEY"] = os.urandom(32)
+app.config["WTF_CSRF_ENABLED"] = True
+csrf = CSRFProtect(app)
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    return render_template("login.html", form=form)
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5010, debug=True)
-
-
