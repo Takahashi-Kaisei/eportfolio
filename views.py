ebe498c9f96858e89c2f2f768a9befdd6201dba7
Takahashi-Kaisei <s2322037@stu.musashino-u.ac.jp>
2024-10-24 10:45:11 +0900

    session["msg"]をflashに置き換えた

--- views.py
+++ views.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, redirect, session
+from flask import Flask, render_template, url_for, redirect, session, flash
 from flask_bootstrap import Bootstrap
 from flask_wtf.csrf import CSRFProtect
 from forms import *
@@ -20,12 +20,12 @@ def login():
     form = LoginForm()
     if form.validate_on_submit():
         if form.password.data != "password":
-            session["msg"] = "Username or Password is incorrect."
+            flash("Username or Password is incorrect.", "danger")
             return redirect(url_for("login"))
         return redirect(url_for("index"))
     msg = session.get("msg")
     session.pop("msg", None)
-    return render_template("login.html", form=form, msg=msg)
+    return render_template("login.html", form=form)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5010, debug=True)
