5db8de83ae440085e3bb2f6c8005a70f3401130c
Takahashi-Kaisei <s2322037@stu.musashino-u.ac.jp>
2024-10-24 11:54:42 +0900

    models.pyを修正

--- models.py
+++ models.py
@@ -4,24 +4,47 @@ class User:
         self.username = None
         self.password = None
     def __repr__(self):
-        return "<User %r>" % self.id
-class Item:
-    def __init__(self):
-        self.id = None
-        self.owner_id = None
-        self.itemname = None
-        self.price = None
-    def __repr__(self):
-        return "<Item %r>" % self.id
+        return "<User %r>" % self.username
 
-class Order:
+class Study:
     def __init__(self):
         self.id = None
-        self.order_code = None
         self.user_id = None
-        self.item_id = None
-        self.amount = None
-        self.price = None
-
+        self.institution = None
+        self.field_of_study = None  # 専攻
+        self.start_date = None  # 開始日
+        self.end_date = None  # 終了日
     def __repr__(self):
-        return "<Order %r>" % self.id
+        return "< The user studied %r at %r>" % (self.field_of_study, self.institution)
+
+class Learninglog:
+    def __init__(self):
+        self.id = None
+        self.user_id = None
+        self.institution = None  # 学校名または教育機関名
+        self.field_of_study = None  # 専攻
+        self.content = None
+        self.date = None
+    def __repr__(self):
+        return "<Learning %r at %r>" % (self.content, self.date)
+
+class Achievement:
+    def __init__(self):
+        self.id = None
+        self.user_id = None
+        self.content = None
+        self.start_date = None  # 開始日
+        self.end_date = None  # 終了日
+        self.description = None  # 詳細説明
+    def __repr__(self):
+        return "<Achievement: %r>" % self.content
+
+class Comment:
+    def __init__(self):
+        self.id = None
+        self.user_id = None
+        self.commnter_name = None
+        self.content = None
+        self.comment_date = None
+    def __repr__(self):
+        return "<Comment %r>" % self.id
