82ccde1c45d55fe510ba26569e7250c0bd934ee4
Takahashi-Kaisei <s2322037@stu.musashino-u.ac.jp>
2024-10-24 11:27:15 +0900

    mdels.pyを作成

---
+++ models.py
@@ -0,0 +1,27 @@
+class User:
+    def __init__(self):
+        self.id = None
+        self.username = None
+        self.password = None
+    def __repr__(self):
+        return "<User %r>" % self.id
+class Item:
+    def __init__(self):
+        self.id = None
+        self.owner_id = None
+        self.itemname = None
+        self.price = None
+    def __repr__(self):
+        return "<Item %r>" % self.id
+
+class Order:
+    def __init__(self):
+        self.id = None
+        self.order_code = None
+        self.user_id = None
+        self.item_id = None
+        self.amount = None
+        self.price = None
+
+    def __repr__(self):
+        return "<Order %r>" % self.id
